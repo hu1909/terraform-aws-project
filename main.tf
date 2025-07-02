0c0cd5a9e47f70e80fcdd33ab9aee457f39f07a1
hu1909 <hugn.nq1909@gmail.com>
2025-07-02 17:57:40 +0000

    Working with EKS (In Progress)

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "vpc" {
 
   name = "klr-vpc"
   cidr = "10.0.0.0/16"
-  
+  azs = [ "ap-southeast-2a", "ap-southeast-2b" ]
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   vpc_flow_log_iam_role_name = "vpc-role"
   vpc_flow_log_iam_role_use_name_prefix = false
@@ -17,8 +17,8 @@ module "vpc" {
 }
 
 ### Security Groups 
-resource "aws_security_group" "allow_webhook" {
-  name = "allow-webhook"
+resource "aws_security_group" "allow-webhook" {
+  name = "allow-webhook-sg"
   description = "Allow Webhook to communicate with Jenkins"
   vpc_id = module.vpc.vpc_id 
 
@@ -40,6 +40,34 @@ resource "aws_security_group" "allow_webhook" {
 
 }
 
+resource "aws_security_group" "allow-ssh" {
+	name = "allow-ssh-sg"
+	description = "Allow SSH to instance"
+	vpc_id = module.vpc.vpc_id 
+
+	ingress {
+		from_port = 22 
+		to_port = 22 
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+  
+}
+
+resource "aws_security_group" "allow-jenkins-access" {
+	name = "allow-jenkins-access"
+	description = "Custom Port for Jenkins"
+
+	ingress {
+		from_port = 8080
+		to_port = 8080
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+  
+}
+
+
 
 ## Define other components 
 data "aws_ami" "amzn-linux-2023-ami" {
@@ -59,11 +87,16 @@ resource "aws_instance" "jenkins-server" {
 	instance_type = "t2.large"  
 	subnet_id = module.vpc.public_subnets[0]
 	user_data = templatefile("${path.module}/install_jenkins.sh", {})
+	security_groups = [aws_security_group.allow-jenkins-access.id, aws_security_group.allow-ssh.id, aws_security_group.allow-webhook.id]
+	
+	# Define the key-pair for SSH 
 
+	
 	tags = {
 	  "server" = "Jenkins"
 	}
 }
+
 
 
 ### EC2 instance for Monitoring 
@@ -72,6 +105,9 @@ resource "aws_instance" "monitoring-server" {
 	instance_type = "t2.large"
 	subnet_id = module.vpc.public_subnets[0]
 	user_data = templatefile("${path.module}/install_monitoring.sh", {})
+	security_groups = [ aws_security_group.allow-ssh.id ]
+
+	# Define the key-pair for SSH 
 
 	tags = {
 	  "server" = "Monitoring"
@@ -90,31 +126,94 @@ resource "aws_ecr_repository" "klr-repo" {
 }
 
 ### ALB
+module "s3-bucket-access" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "5.1.0"
+  bucket = "access_logs_klr_alb"
+  versioning = {
+    enabled = true
+  }
+  attach_elb_log_delivery_policy = true  
+  attach_lb_log_delivery_policy  = true
+}
+
+module "s3-bucket-connection" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "5.1.0"
+  bucket = "connection_logs_klr_alb"
+  versioning = {
+    enabled = true
+  }
+  attach_elb_log_delivery_policy = true  
+  attach_lb_log_delivery_policy  = true
+  
+}
+
 resource "aws_lb" "alb-klr" {
 	name = "alb-klr"
+
+	# Add security group 
+
 	load_balancer_type = "application"
 	subnet_mapping {
 		subnet_id = module.vpc.public_subnets[1]
 	}
 
 	access_logs {
-	  bucket = "access_logs_klr_alb"
+	  bucket = module.s3-bucket-access.s3_bucket_id
 	  enabled = true 
-	  prefix = "klr-alb"
+	  prefix = "klr-alb-access"
 	}
 
 	connection_logs {
-	  bucket = "connection_logs_klr_alb"
+	  bucket = module.s3-bucket-connection.s3_bucket_id
 	  enabled = true 
 	  prefix = "klr-alb-connection"
 	}
 
 }
 
+
 ### Route 53 
+
 
 
 ### ACM
 
 
 ## Define the EKS within the ALB
+module "eks_managed_node_groups" {
+  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+
+  name = "eks-node-groups"
+  cluster_name = "klr-cluster"
+  cluster_version = "1.32"
+
+  cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
+  vpc_security_group_ids            = [module.eks.node_security_group_id]
+
+#   remote_access = {
+#     ec2_ssh_key               = module.key_pair.key_pair_name
+#     source_security_group_ids = [aws_security_group.remote_access.id]
+#   }
+  min_size     = 1
+  max_size     = 10
+  desired_size = 1
+
+  instance_types = ["t2.large"]
+
+  labels = {
+    Environment = "test"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  taints = {
+    dedicated = {
+      key    = "dedicated"
+      value  = "gpuGroup"
+      effect = "NO_SCHEDULE"
+    }
+  }
+}
+
--- providers.tf
+++ providers.tf
@@ -2,13 +2,13 @@ terraform {
 	required_providers {
 		aws = {
 			source = "hashicorp/aws"
-			version = "~>5.0"
+			version = "~> 5.95.0"
 		}
 	}
 
 	backend "s3" {
 	  bucket = "terraform-state-hu1909"
-	  key = "/state/terraform.tfstate"
+	  key = "state/terraform.tfstate"
 	  region = "ap-southeast-2"
 	  encrypt = true
 	}
